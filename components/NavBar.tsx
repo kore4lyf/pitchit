18ad334fe4c46918b7798c31c6385e5136f61120
Korede Faleye <akfaleye@gmail.com>
2025-03-29 17:12:57 +0100

    Style nav menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,6 +3,9 @@ import Image from "next/image"
 import Link from "next/link"
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Menubar, MenubarContent, MenubarMenu, MenubarRadioGroup, MenubarTrigger, MenubarItem, MenubarSeparator } from "./ui/menubar";
+import { LogOut, PlusCircle } from "lucide-react";
 
 const NavBar = async () => {
   const session = await auth();
@@ -15,27 +18,41 @@ const NavBar = async () => {
         </Link>
 
         <div  className="flex gap-5">
-          <div className="flex  item-center gap-5 text-black-200">   
+          <Link href="/startup/create" className="flex gap-2 items-center">
+            Create <PlusCircle className="size-4 text-green-500"/>
+          </Link>
+
+          <div>   
             {session && session?.user ? (
               <>
-                <Link href="/startup/create">
-                  <span>Create</span>
-                </Link>
+                <Menubar className=" border-0 shadow-none">
+                  <MenubarMenu>
+                  <MenubarTrigger className="flex gap-2">
+                    <span> {session?.user?.name} </span>
+                    <Avatar className="w-fit h-fit">
+                      <AvatarImage src={session?.user?.image} className="w-7 h-7 rounded_full outline outline-2 outline-gray-300 hover:outline-customTeal outline-offset-1"/>
+                      <AvatarFallback className="border rounded_full p-2 w-8 h-8">{session?.user.name !== undefined && session?.user.name.slice(0,2).toUpperCase()}</AvatarFallback>
+                    </Avatar>
+                  </MenubarTrigger>
+                
+                  <MenubarContent className="font-work-sans">
 
-                <form action={async () => {
-                  "use server"
-                  await signOut({redirectTo: "/"})
-                }}>
-                  <button type="submit"> Logout </button>
-                </form>
-                {console.log(session)}
-                <Link href={`/user/${session?.id}`} className="flex items-center">
-                  <span> {session?.user?.name} </span>
-                  <Avatar className="w-fit h-fit">
-                    <AvatarImage src={session?.user?.image} className="w-8 h-8 rounded_full outline outline-2 outline-gray-300 hover:outline-customTeal outline-offset-1"/>
-                    <AvatarFallback className="border rounded_full p-2 w-8 h-8">{session?.user.name !== undefined && session?.user.name.slice(0,2).toUpperCase()}</AvatarFallback>
-                  </Avatar>
-                </Link>
+                  <MenubarItem inset>
+                    <Link href={`/user/${session?.id}`} className="flex items-center font-medium"> Profile </Link>
+                  </MenubarItem>
+                  <MenubarSeparator />
+
+                  <MenubarItem inset className="hover:text-red-600">
+                    <form action={async () => {
+                      "use server"
+                      await signOut({redirectTo: "/"})
+                    }}>
+                      <button type="submit" className="flex gap-2 items-center"> Logout <LogOut className="size-4 text-red-600" /> </button>
+                    </form>
+                  </MenubarItem>
+                  </MenubarContent>
+                  </MenubarMenu>
+                </Menubar>
               </>
             ):(
               <form action={async () => {
